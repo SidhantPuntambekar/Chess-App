5c20e19f51cbe1f19ec8c2c61cafb9350f2593ba
Sidhant Puntambekar <sidhantnp@yahoo.com>
2022-07-24 18:06:15 -0600

    Added emptyTile subclass of Tile

--- src/Tile.java
+++ src/Tile.java
@@ -13,3 +13,24 @@ public abstract class Tile {
     /* Public abstract method to determine if tile is occupied by a piece */
     public abstract Piece getPiece();
 }
+
+/* Empty tile static class */
+public final class emptyTile extends Tile {
+    /* Empty Tile Constructor */
+    public emptyTile(int tileNum) {
+        super(tileNum);
+    }
+
+    /* isOccupied override method: Tile is unoccupied */
+    @Override
+    public boolean isOccupied() {
+        return false;
+    }
+
+    /* getPiece override method: No piece on empty tile */
+    @Override
+    public Piece getPiece() {
+        return null;
+    }
+}
+
