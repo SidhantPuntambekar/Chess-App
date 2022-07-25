24608296b78f85ebce153b963804fbfe6fa59bdb
Sidhant Puntambekar <sidhantnp@yahoo.com>
2022-07-24 18:16:37 -0600

    Created piece class and occupiedTile class

--- src/Tile.java
+++ src/Tile.java
@@ -12,25 +12,51 @@ public abstract class Tile {
 
     /* Public abstract method to determine if tile is occupied by a piece */
     public abstract Piece getPiece();
+
+    /* Empty tile class */
+    public static final class emptyTile extends Tile
+    {
+        /* Empty Tile Constructor */
+        public emptyTile(int tileNum) {
+            super(tileNum);
+        }
+
+        /* isOccupied override method: Tile is unoccupied */
+        @Override
+        public boolean isOccupied() {
+            return false;
+        }
+
+        /* getPiece override method: No piece on empty tile */
+        @Override
+        public Piece getPiece() {
+            return null;
+        }
+    }
+
+    /* Occupied tile class */
+    public static final class occupiedTile extends Tile
+    {
+        private Piece occupiedPiece; // Create private reference to piece occupying tile
+        /* Occupied Tile Constructor */
+        public occupiedTile(int tileNum, Piece occupiedPiece)
+        {
+            super(tileNum);
+            this.occupiedPiece = occupiedPiece;
+        }
+
+        /* isOccupied override method: Tile is occupied */
+        @Override
+        public boolean isOccupied() {
+            return true;
+        }
+
+        /* getPiece override method: No piece on empty tile */
+        @Override
+        public Piece getPiece() {
+            return this.occupiedPiece;
+        }
+    }
 }
 
-/* Empty tile static class */
-public final class emptyTile extends Tile {
-    /* Empty Tile Constructor */
-    public emptyTile(int tileNum) {
-        super(tileNum);
-    }
-
-    /* isOccupied override method: Tile is unoccupied */
-    @Override
-    public boolean isOccupied() {
-        return false;
-    }
-
-    /* getPiece override method: No piece on empty tile */
-    @Override
-    public Piece getPiece() {
-        return null;
-    }
-}
 
